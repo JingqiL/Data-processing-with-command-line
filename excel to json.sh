7b178951ea514555ba313091509bb616510f619f
Jingqi <jingqliu@umich.edu>
2017-06-28 14:46:30 -0400

    Update and rename excel to json.sh to excel&csv to json.sh

--- excel to json.sh
+++
@@ -1,5 +0,0 @@
-#!/bash/bin
-#if you did not install json-cli before, install it from npm. Installation: npm install -g json-cli
-#Details: https://www.npmjs.com/package/json-cli
-
-json-cli file.xls file.json
---
+++ excel&csv to json.sh
@@ -0,0 +1,11 @@
+#!/bash/bin
+#Install json-cli from npm. Installation: npm install -g json-cli
+#Details: https://www.npmjs.com/package/json-cli
+
+json-cli file.xls file.json
+
+#Install csv2json from npm. Installation: npm install --global csv2json
+#Details: https://www.npmjs.com/package/csv2json
+
+csv2json file.csv file.json
+
