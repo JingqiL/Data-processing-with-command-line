fa38ad3e27a76223f3c8be19272c4d9125ccc700
Jingqi <jingqliu@umich.edu>
2017-06-28 14:38:45 -0400

    Create excel to json.sh

---
+++ excel to json.sh
@@ -0,0 +1,5 @@
+#!/bash/bin
+#if you did not install json-cli before, install it from npm. Installation: npm install -g json-cli
+#Details: https://www.npmjs.com/package/json-cli
+
+json-cli file.xls file.json
