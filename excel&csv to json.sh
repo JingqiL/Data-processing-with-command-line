8ffc021f022076d38566f8e7a7cf7301dfd817ad
Jingqi <jingqliu@umich.edu>
2017-07-13 09:30:47 -0400

    Update excel&csv to json.sh

--- excel&csv to json.sh
+++ excel&csv to json.sh
@@ -2,10 +2,10 @@
 #Install json-cli from npm. Installation: npm install -g json-cli
 #Details: https://www.npmjs.com/package/json-cli
 
-json-cli file.xls file.json
-
+json-cli file.xls file.json #Have a limitation of size 60 MB.
+ 
 #Install csv2json from npm. Installation: npm install --global csv2json
 #Details: https://www.npmjs.com/package/csv2json
 
-csv2json file.csv file.json
+csv2json file.csv file.json 
 
