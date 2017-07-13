89e2a37ac42f203328def3a11b76fad38874253b
Jingqi <jingqliu@umich.edu>
2017-07-13 17:01:55 -0400

    Update jq.sh

--- jq.sh
+++ jq.sh
@@ -1,1 +1,7 @@
-
+# !/bin/bash
+jq '.' file.json #Spread .json to a well-formatted json file.
+jq '.[]' file.json #Spread .json to a well-formatted ndjson file.
+jq '.[] | {Var1: .var1, Var2: .var2}' file.json #Extract Items var1 and var2 from the previous json file as an ndjson.
+jq '.[] | {Var1: .var1, Var2: .var2[].Items}' file.json #If var2 is an array then spread it and extract object 'Items' from object var2.
+jq -s . file.json #Make a well-formatted json back to a one-line json.
+jq 
