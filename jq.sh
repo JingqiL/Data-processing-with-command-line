c34e2080b3cdd5051a34c2cacfa1da2e4fc9c4ae
Jingqi <jingqliu@umich.edu>
2017-07-18 17:18:04 -0400

    Update jq.sh

--- jq.sh
+++ jq.sh
@@ -4,4 +4,8 @@ jq '.[]' file.json #Spread .json to a well-formatted ndjson file.
 jq '.[] | {Var1: .var1, Var2: .var2}' file.json #Extract Items var1 and var2 from the previous json file as an ndjson.
 jq '.[] | {Var1: .var1, Var2: .var2[].Items}' file.json #If var2 is an array then spread it and extract object 'Items' from object var2.
 jq -s . file.json #Make a well-formatted json back to a one-line json.
-jq 
+#Use map and group_by to do simple calculation on data. 
+jq '[.] | map({Country: .pubcountry}) | .[]' 1.ndjson| jq -s . | jq 'group_by(.Country) | map({Country: .[].Country, Count: length}) | unique' > 2.json
+#This code counts the number of Items which pubcounty
+jq -c '.' file #Make well-formatted .ndjson back to one line .ndjson.
+
