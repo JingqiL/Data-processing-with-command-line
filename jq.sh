8de65f3593e8c90caadbcd49307d9990dd0f5e99
Jingqi <jingqliu@umich.edu>
2019-02-27 12:05:55 -0800

    Update jq.sh

--- jq.sh
+++ jq.sh
@@ -1,11 +1,27 @@
 # !/bin/bash
-jq '.' file.json #Spread .json to a well-formatted json file.
-jq '.[]' file.json #Spread .json to a well-formatted ndjson file.
-jq '.[] | {Var1: .var1, Var2: .var2}' file.json #Extract Items var1 and var2 from the previous json file as an ndjson.
-jq '.[] | {Var1: .var1, Var2: .var2[].Items}' file.json #If var2 is an array then spread it and extract object 'Items' from object var2.
-jq -s . file.json #Make a well-formatted json back to a one-line json.
+
+#Spread .json to a well-formatted json file.
+jq '.' file.json 
+
+#Spread .json to a well-formatted ndjson file.
+jq '.[]' file.json
+
+#Extract Items var1 and var2 from the previous json file as an ndjson.
+jq '.[] | {Var1: .var1, Var2: .var2}' file.json 
+
+#If var2 is an array then spread it and extract object 'Items' from object var2.
+jq '.[] | {Var1: .var1, Var2: .var2[].Items}' file.json
+
+#Make a well-formatted json back to a one-line json.
+jq -s . file.json
+
 #Use map and group_by to do simple calculation on data. 
-jq '[.] | map({Country: .pubcountry}) | .[]' 1.ndjson| jq -s . | jq 'group_by(.Country) | map({Country: .[].Country, Count: length}) | unique' > 2.json
 #This code counts the number of Items which pubcounty
-jq -c '.' file #Make well-formatted .ndjson back to one line .ndjson.
+jq '[.] | map({Country: .pubcountry}) | .[]' 1.ndjson| jq -s . | jq 'group_by(.Country) | map({Country: .[].Country, Count: length}) | unique' > 2.json
+
+#Make well-formatted .ndjson back to one line .ndjson.
+jq -c '.' file
+
+#export .ndjson to .csv
+jq '. | [.Var1,.Var2] | @csv' file.ndjson | 
 
